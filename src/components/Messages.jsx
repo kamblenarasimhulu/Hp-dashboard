daefc881d079e204af9b14796db5e51bf0ccdb50
kamble <d.dubbledam@email.com>
2024-05-10 20:56:03 +0530

    first commit

---
+++ src/components/Messages.jsx
@@ -0,0 +1,57 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { Context } from '../main'
+import axios from 'axios';
+import { Navigate } from 'react-router-dom';
+
+const Messages = () => {
+
+  const [messages,setMessages]=useState([]);
+  const {isAuthenticated}=useContext(Context);
+
+  useEffect(()=>{
+    const fetchMessages = async()=>{
+      try {
+        const {data}=await axios.get("",
+        {withCredentials:true}
+      );
+      setMessages(data.messages);
+        
+
+      } catch (error) {
+        console.log("ERROR OCCURED WHILE FETCHING MESSAGES",error);
+      }
+     
+    }
+    fetchMessages();
+  },[]);
+
+  if(isAuthenticated){
+    return<Navigate to={"/login"}/>;
+  }
+  return (
+    <>
+    <section>
+      <h1>MESSAGES</h1>
+      <div className='banner'>
+      {
+        messages && messages.length>0 ? (messages.map(element=>{
+          return(
+            <div className="card">
+              <div className="details">
+                <p>First Name:<span>{element.firstName}</span></p>
+                <p>Last Name:<span>{element.lastName}</span></p>
+                <p>Email:<span>{element.email}</span></p>
+                <p>Phone:<span>{element.phone}</span></p>
+                <p>Message:<span>{element.message}</span></p>
+              </div>
+            </div>
+          )
+        })) : (<h1>No Messagges!</h1>)
+      }
+      </div>
+      </section>      
+    </>
+  )
+}
+
+export default Messages
